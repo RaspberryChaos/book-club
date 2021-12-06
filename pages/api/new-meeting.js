023b3fa8e6522cc79b6cbc324b7cb5694ed5efd3
Ailie McCorkindale <ailiem134@hotmail.com>
2021-12-06 23:57:59 +0100

    error handling added to api

--- pages/api/new-meeting.js
+++ pages/api/new-meeting.js
@@ -3,21 +3,32 @@ import { MongoClient } from "mongodb";
 async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-
     const user = process.env.DB_USER;
     const password = process.env.DB_PASS;
 
-    const client = await MongoClient.connect(
-      `mongodb+srv://${user}:${password}@cluster0.xaika.mongodb.net/bookClubMeetings?retryWrites=true&w=majority`
-    );
-    const db = client.db();
+    let client;
 
-    const meetingsCollection = db.collection("meetings");
+    try {
+      client = await MongoClient.connect(
+        `mongodb+srv://${user}:${password}@cluster0.xaika.mongodb.net/bookClubMeetings?retryWrites=true&w=majority`
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database." });
+      return;
+    }
 
-    const result = await meetingsCollection.insertOne(data);
-    console.log(result);
+    let db = client.db();
+
+    try {
+      const result = await db.collection("meetings").insertOne(data);
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Failed to add meetup!" });
+      return;
+    }
+
+    //console.log(result);
     client.close();
-
     res.status(201).json({ message: "Meeting added!" });
   }
 }
