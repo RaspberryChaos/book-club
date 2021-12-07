de2ee579c431fe0d4340375723156d1902996cd4
Ailie McCorkindale <ailiem134@hotmail.com>
2021-12-07 23:37:59 +0100

    database calls in get static paths and get static props functions

--- pages/meetings/[meetingId].js
+++ pages/meetings/[meetingId].js
@@ -1,39 +1,70 @@
-const MeetingDetails = (props) => {
+import { MongoClient, ObjectId } from "mongodb";
+
+const MeetingDetails = ({ title, author, address, date, description, id }) => {
   return (
     <section>
       <h1>Meeting Details Page</h1>
-      <p>Book Title</p>
-      <p>{props.id}</p>
-      <p>Author</p>
-      <p>Date</p>
-      <p>Address</p>
-      <p>Description</p>
+      <p>{title}</p>
+      <p>{id}</p>
+      <p>{author}</p>
+      <p>{date}</p>
+      <p>{address}</p>
+      <p>{description}</p>
     </section>
   );
 };
 
 export async function getStaticPaths() {
+  const user = process.env.DB_USER;
+  const password = process.env.DB_PASS;
+
+  const client = await MongoClient.connect(
+    `mongodb+srv://${user}:${password}@cluster0.xaika.mongodb.net/bookClubMeetings?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+  const meetingsCollection = db.collection("meetings");
+
+  const meetings = await meetingsCollection
+    .find()
+    .project({ _id: 1 })
+    .toArray();
+
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: { meetingId: "meeting_1" },
-      },
-      {
-        params: { meetingId: "meeting_2" },
-      },
-      {
-        params: { meetingId: "meeting_3" },
-      },
-    ],
+    paths: meetings.map((meeting) => ({
+      params: { meetingId: meeting._id.toString() },
+    })),
   };
 }
+
 export async function getStaticProps(context) {
   const meetingId = context.params.meetingId;
+
+  const user = process.env.DB_USER;
+  const password = process.env.DB_PASS;
+
+  const client = await MongoClient.connect(
+    `mongodb+srv://${user}:${password}@cluster0.xaika.mongodb.net/bookClubMeetings?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+  const meetingsCollection = db.collection("meetings");
+  const meeting = await meetingsCollection.findOne({
+    _id: ObjectId(meetingId),
+  });
+  client.close();
+
   return {
     props: {
-      id: meetingId,
+      title: meeting.title,
+      author: meeting.author,
+      address: meeting.address,
+      date: meeting.date,
+      description: meeting.description,
+      id: meeting._id.toString(),
     },
+    revalidate: 60,
   };
 }
 
